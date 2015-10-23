c3fa0dd0aaa2a6fd774d1f6d3af6fcab9d4b5665
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-10-23 17:00:23 +0300

    Basic input from Scanner

---
+++ 01_SampleJava/src/class_001/TextInput.java
@@ -0,0 +1,15 @@
+package class_001;
+
+import java.util.Scanner;
+
+public class TextInput {
+	public static void main(String [] args){
+		System.out.print("Enter your name: ");
+//		standard input from Scanner(System.in)
+		final Scanner in = new Scanner(System.in);
+//		get the name (read from System.in)
+		final String name = in.next();
+//		output the name
+		System.out.println("Hello, " + name);
+	}
+}
