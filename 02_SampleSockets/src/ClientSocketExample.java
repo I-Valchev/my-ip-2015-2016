0665a4afcaf55a1fe2c740cf7b1034753040c3d0
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-11-06 16:51:43 +0200

    Aded a variable to hold System.out

--- 02_SampleSockets/src/ClientSocketExample.java
+++ 02_SampleSockets/src/ClientSocketExample.java
@@ -1,12 +1,15 @@
+import java.io.PrintStream;
 
 public class ClientSocketExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GET / HTTP/1.1");
-		System.out.println("Host:www.example.com");
-		System.out.println("Connection:close");
-		System.out.println("");
+		
+		final PrintStream out = System.out;
+		out.println("GET / HTTP/1.1");
+		out.println("Host:www.example.com");
+		out.println("Connection:close");
+		out.println("");
 	}
 
 }
