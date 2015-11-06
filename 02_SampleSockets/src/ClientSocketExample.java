105212b47388f5e77b6fe48793939fb08caaa439
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-11-06 16:50:19 +0200

    Added sample HTTP request, printed to console

---
+++ 02_SampleSockets/src/ClientSocketExample.java
@@ -0,0 +1,12 @@
+
+public class ClientSocketExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GET / HTTP/1.1");
+		System.out.println("Host:www.example.com");
+		System.out.println("Connection:close");
+		System.out.println("");
+	}
+
+}
